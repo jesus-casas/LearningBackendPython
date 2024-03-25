064cdc1a485ba84ff43aed80f0fb3c84a5da2573
jesus-casas <javiercasas740@gmail.com>
2024-03-25 14:51:42 -0700

    adding main.py to repository

---
+++ main.py
@@ -0,0 +1,83 @@
+# Hello these are the baic operations of python!
+# This is a comment
+
+"""
+    This is a multi-line comment
+"""
+#this is how to print a string with each printing in a new line
+print("Hello World")
+print('Hello World')
+print(5 + 1)    
+
+print("=====================================")
+# Variables in python typical noted like this 
+my_variable = 5
+print(my_variable)
+
+my_variable = 5
+my_variable = 10
+print(my_variable) # This will print 10
+
+print("=====================================")
+# Math operations
+a, b = 1, 2
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+
+print(sum)
+print(difference)
+print(product)
+print(quotient)
+
+print("=====================================")
+# Types of variables
+name_with_double_quotes = "boot.dev"
+# Integer
+x = 5
+y = -5
+# Float
+z = 5.2
+# Boolean
+is_true = True
+is_false = False
+
+sword_name, sword_damage, sword_length = "Excalibur", 10, 200
+
+print("=====================================")
+# f-strings in python way to put variables in a string
+name = "Yarl"
+age = 37
+race = "dwarf"
+
+print(f"{name} is a {race} who is {age} years old.")
+print("=====================================")
+
+# create none variable in python or often refered to as null in other languages
+enemy = None
+print(enemy is None)
+
+# Math with strings 
+first_name = "Lane "
+last_name = "Wagner"
+full_name = first_name + last_name
+print(full_name)
+# prints "Lane Wagner"
+print("=====================================")
+
+print("=====================================")
+
+print("=====================================")
+
+print("=====================================")
+
+print("=====================================")
+
+print("=====================================")
+
+print("=====================================")
+
+print("=====================================")
+
+print("=====================================")
