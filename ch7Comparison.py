ce3b53d2f09b217365d34cc52b0db02e82728087
jesus-casas <javiercasas740@gmail.com>
2024-03-27 14:11:59 -0700

    Adding unfinished notes from chapter 5

--- ch7Comparison.py
+++ ch7Comparison.py
@@ -1,2 +1,17 @@
 # Chapter 7 Comparison Operators
 # Objective: This program demonstrates the use of comparison operators in Python.
+5 < 6 # evaluates to True
+5 > 6 # evaluates to False
+5 >= 6 # evaluates to False
+5 <= 6 # evaluates to True
+5 == 6 # evaluates to False
+5 != 6 # evaluates to True
+
+#Example #2 
+def compare_heights(edward_height, alphonse_height, winry_height, mustang_height):
+    is_mustang_edward_same = edward_height == mustang_height
+    is_alphonse_edward_same = edward_height == alphonse_height
+    is_winry_alphonse_same = winry_height == alphonse_height
+    return is_mustang_edward_same, is_alphonse_edward_same, is_winry_alphonse_same
+
+
