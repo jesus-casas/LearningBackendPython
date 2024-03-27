9c01a2e0484b815d6acf199d11d1b25d0731335c
jesus-casas <javiercasas740@gmail.com>
2024-03-27 14:49:40 -0700

    Uploading finished chapter 7

--- ch7Comparison.py
+++ ch7Comparison.py
@@ -14,4 +14,27 @@ def compare_heights(edward_height, alphonse_height, winry_height, mustang_height
     is_winry_alphonse_same = winry_height == alphonse_height
     return is_mustang_edward_same, is_alphonse_edward_same, is_winry_alphonse_same
 
+# IF statement
+def print_status(player_health):
+    if player_health == 0:
+        print("dead")
+    print("status check complete")
 
+# is in operator
+
+# If-else statement
+if score > high_score:
+    print('High score beat!')
+elif score > second_highest_score:
+    print('You got second place!')
+elif score > third_highest_score:
+    print('You got third place!')
+else:
+    print('Better luck next time')
+
+#Tip IF statement dont need a comparison operator
+# if is_big:
+    # ...
+# if is_big == True:
+    # ...
+# Both act the same way
