fb9eb0578126ac24426658212711fd2313f1141b
jesus-casas <javiercasas740@gmail.com>
2024-03-27 15:47:27 -0700

    finished chapter 8 loops and adding chapter 9 file

---
+++ Ch8Loops.py
@@ -0,0 +1,32 @@
+# Chapter 4 Loops
+for i in range(0, 10):
+    print(i)
+# Output: 0 1 2 3 4 5 6 7 8 9
+# automatically increments by 1
+# range(start, stop, step)
+# stops at stop - 1
+
+for i in range(0, 10, 2):
+    print(i)
+# prints:
+# 0
+# 2
+# 4
+# 6
+# 8
+
+for i in range(3, 0, -1):
+    print(i)
+# prints:
+# 3
+# 2
+# 1
+
+# In this example, the function sum_of_odd_numbers takes in an integer end 
+# and returns the sum of all odd numbers from 0 to end - 1.
+def sum_of_odd_numbers(end):
+    total = 0
+    for i in range(0, end):
+        if i % 2 == 1:
+            total += i
+    return total
