60205c570d6e175ee00758b0b728414b0d9668ec
jesus-casas <javiercasas740@gmail.com>
2024-03-28 22:51:05 -0700

    Modified chapter 4 to be more user friendly and added description on top of file.

--- LearnPython/ch4Scope.py
+++ LearnPython/ch4Scope.py
@@ -1,13 +1,25 @@
 # Chapter 4 Scope 
-# Trying to access a variable outside of its scope will result in an error
-def subtract(x, y):
-    return x - y
-result = subtract(5, 3)
-print(x)
-# ERROR! "name 'x' is not defined"
+# I learned in this chapter trying to access a variable outside of its scope will result in an error
+# My notes to help me quickly reference the syntax and concepts of scope in Python.
 
-# Correct way to access the result
-pi = 3.14
+def main():
+    # Example 1: Subtracting two numbers
+    def subtract(x, y):
+        return x - y
 
-def get_area_of_circle(radius):
-    return pi * radius * radius
+    # If we try to access the x or y variable here, it will result in an error
+    # because they are not defined in this scope.
+    result = subtract(5, 3)
+    print(f"Result of subtraction: {result}")
+
+    # Example 2: Calculating the area of a circle
+    pi = 3.14
+
+    def get_area_of_circle(radius):
+        return pi * radius * radius
+
+    area = get_area_of_circle(5)
+    print(f"Area of the circle: {area}")
+
+if __name__ == "__main__":
+    main()
