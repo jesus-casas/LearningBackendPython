9dd010992685a81136f31b925bf6d25250748dce
jesus-casas <javiercasas740@gmail.com>
2024-03-30 11:26:27 -0700

    Updating chapter 8 file with better syntax and easier to read output

--- LearnPython/ch8Loops.py
+++ LearnPython/ch8Loops.py
@@ -1,11 +1,18 @@
-# Chapter 4 Loops
+# Chapter 8 Loops
+# I learned in this chapter how to use loops in Python to repeat code blocks.
+# My notes to help me quickly reference the syntax and concepts of loops in Python.
+
+# automatically increments by 1
+# range(start, stop, step)
+
+print("For loop: from 0 to 9")
 for i in range(0, 10):
     print(i)
 # Output: 0 1 2 3 4 5 6 7 8 9
-# automatically increments by 1
-# range(start, stop, step)
 # stops at stop - 1
 
+print("=====================================")
+print("For loop: from 0 to 9 with step 2")
 for i in range(0, 10, 2):
     print(i)
 # prints:
@@ -15,6 +22,8 @@ for i in range(0, 10, 2):
 # 6
 # 8
 
+print("=====================================")
+print("For loop: from 3 to 0 with step -1")
 for i in range(3, 0, -1):
     print(i)
 # prints:
@@ -31,3 +40,11 @@ def sum_of_odd_numbers(end):
             total += i
     return total
 
+# Main function calls sum_of_odd_numbers with argument 10
+def main():
+    end = 10 #ends at 10 - 1 = 9
+    result = sum_of_odd_numbers(10)
+    print(f"Sum of odd numbers from 0 to {end - 1}: {result}")
+
+if __name__ == "__main__":
+    main()
