83344f831617fd311445defaab50db75b7a11c24
jesus-casas <javiercasas740@gmail.com>
2024-04-01 21:48:20 -0700

    adding more examples for ch9 lists. Examples include counting elements in a list and for printing an array with no index syntax

--- LearnPython/ch9Lists.py
+++ LearnPython/ch9Lists.py
@@ -38,17 +38,20 @@ def main():
     length = len(best_languages)
     print(length) 
     # prints 5, because there are 5 elements in the list
+    
     print ("=====================================")
     print("Example: List updates")
     best_languages[1] = "Ruby"
     print(best_languages[1])
     # prints "Ruby" now, because we updated the value at index 1 to "Ruby"
+    
     print ("=====================================")
     print("Example: List append")
     best_languages.append("Go")
     print(best_languages)
     # prints ["JavaScript", "Ruby", "Rust", "Python", "C", "Go"]
     # because we added "Go" to the end of the list with .append()
+    
     print ("=====================================")
     print("Example: List pop")
     vegetables = ["broccoli", "cabbage", "kale", "tomato"];
@@ -57,8 +60,33 @@ def main():
     # vegetables = ['broccoli', 'cabbage', 'kale']
     # last_vegetable = 'tomato'
     
-
-
+    print ("=====================================")
+    print("Example: Counting elements in a list")
+    broccoli_count = 0
+    cabbage_count = 0
+    kale_count = 0
+    for i in range(0, len(vegetables)):
+        if vegetables[i] == "broccoli":
+            broccoli_count += 1
+        elif vegetables[i] == "cabbage":
+            cabbage_count += 1
+        elif vegetables[i] == "kale":
+            kale_count +=1
+    print("Broccoli count:", broccoli_count)
+    print("Cabbage count:", cabbage_count)
+    print("Kale count:", kale_count)
+    
+    print ("=====================================")
+    print("Example: No-index Syntax")
+    trees = ['oak', 'pine', 'maple']
+    for tree in trees:
+        print(tree)
+    # Prints:
+    # oak
+    # pine
+    # maple
+    print ("=====================================")
+    
 # This block checks if the script is being run directly on system.
 # If it is run directly, the main() function is called.
 # This is a common pattern used to organize code in Python scripts.
