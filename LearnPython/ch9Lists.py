262bee6608db904babf522711d650e6a9d6ac0b1
jesus-casas <javiercasas740@gmail.com>
2024-04-01 20:57:12 -0700

    Adding basic chapter 9 structure

--- LearnPython/ch9Lists.py
+++ LearnPython/ch9Lists.py
@@ -1,6 +1,18 @@
 # Chapter 9 in Python Course on Lists
 # I learned in this chapter how to use lists in Python to store multiple items in a single variable.
+# My Notes help me to remember the syntax and how to use lists in Python.
 # Called lists not arrays in Python
 
-inventory = ["Iron Breastplate", "Healing Potion", "Leather Scraps"]
+def main():
+    inventory = ["Iron Breastplate", "Healing Potion", "Leather Scraps"]
 
+
+
+
+
+
+# This block checks if the script is being run directly on system.
+# If it is run directly, the main() function is called.
+# This is a common pattern used to organize code in Python scripts.
+if __name__ == "__main__":
+    main()
