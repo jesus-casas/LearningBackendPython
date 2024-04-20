5d8926a431d7f0b3f9a8e23fe779dc043c1915b7
jesus-casas <javiercasas740@gmail.com>
2024-04-20 10:24:42 -0700

    Adding ch10Dictionaries.py file and added basic notes

---
+++ LearnPython/ch10Dictionaries.py
@@ -0,0 +1,7 @@
+# Chapter 10 in Python Course on Dictionaries
+# I learned in this chapter how to use dictionaries in Python to store key-value pairs.
+# My Notes help me to remember the syntax and how to use dictionaries in Python.
+
+# Dictionaries in Python are used to store data values in key -> value pairs. 
+# Dictionaries are a great way to store groups of information.
+
