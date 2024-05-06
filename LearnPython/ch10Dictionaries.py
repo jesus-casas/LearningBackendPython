d60eb4f0b420dbd382747a8cd5ffae188fd55b0d
jesus-casas <javiercasas740@gmail.com>
2024-05-06 13:39:03 -0700

    Finished accessing elements in a dictionary and setting dictionary values examples

--- LearnPython/ch10Dictionaries.py
+++ LearnPython/ch10Dictionaries.py
@@ -15,6 +15,7 @@ def main():
         "year": 2019
     }
     print(car)
+    
     print("=====================================")
     print ("Example: Duplicate Keys")
     # Because dictionaries rely on unique keys, you can't have two of the same key in the same dictionary. 
@@ -28,9 +29,36 @@ def main():
     # The key "brand" is repeated three times, but only the last value is stored in the dictionary.
     
     print("=====================================")
+    print("Example: Accessing elements in a dictionary")
+    # Accessing elements in a dictionary is done by providing the key using square brackets.
+    companies = {
+        "Apple": "iOS",
+        "Google": "Android",
+        "Microsoft": "Windows"
+    }
     
-    
+    print(companies["Apple"]) # prints "iOS"
 
+    print("=====================================")
+    print("Setting Dictionary Values")
+    # You can set the value of a specific key in a dictionary by providing the key and the new value.
+    names = ["jack bronson", "jill mcarty", "john denver"]
+
+    names_dict = {}
+    for name in names:
+        # .split() returns a list of strings
+        # where each string is a single word from the original
+        name_list = name.split()
+
+        # here we update the dictionary
+        # format: dictionary[key] = value
+        names_dict[name_list[0]] = name_list[1]
+
+    print(names_dict)
+    # Prints: {'jack': 'bronson', 'jill': 'mcarty', 'john': 'denver'}
+
+    
+    
 # This block checks if the script is being run directly on system.
 # If it is run directly, the main() function is called.
 # This is a common pattern used to organize code in Python scripts.
