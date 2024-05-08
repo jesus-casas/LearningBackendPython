c0e100174f578cf92d9e0faa611c2aee8fe07a42
jesus-casas <javiercasas740@gmail.com>
2024-05-07 19:54:31 -0700

    Adding ch11Sets.py file with basic example on how to create a set

---
+++ LearnPython/ch11Sets.py
@@ -0,0 +1,28 @@
+# Chapter 11 in Python Course on Sets
+# I learned in this chapter how to use sets in Python to store unique values.
+# My Notes help me to remember the syntax and how to use sets in Python.
+
+# Sets in Python are used to store unique values.
+# Sets are a great way to store groups of information.
+
+def main():
+    print("Chapter 11 in Python Course on Sets")
+    
+    print("=====================================")
+    print("Example: Creating a set")
+    fruits = {'apple', 'banana', 'grape'}
+    print(type(fruits))
+    # Prints: <class 'set'>
+
+    print(fruits)
+    # Prints: {'banana', 'grape', 'apple'}
+
+    
+    print("=====================================")
+    print("End of chapter 11 notes")
+
+# This block checks if the script is being run directly on system.
+# If it is run directly, the main() function is called.
+# This is a common pattern used to organize code in Python scripts.
+if __name__ == "__main__":
+    main()
